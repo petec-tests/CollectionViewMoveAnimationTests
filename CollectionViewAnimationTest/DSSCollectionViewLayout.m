5441bf2c0fc1a666606129b9b04047579303554f
Pete Callaway <pete@dativestudios.com>
2014-05-28 15:16:09 +0100

    Add an empty custom layout

---
+++ CollectionViewAnimationTest/DSSCollectionViewLayout.m
@@ -0,0 +1,15 @@
+//
+//  Created by Pete Callaway on 28/05/2014.
+//  Copyright (c) 2014 Dative Studios. All rights reserved.
+//
+
+#import "DSSCollectionViewLayout.h"
+
+
+@implementation DSSCollectionViewLayout
+
+- (void)prepareForCollectionViewUpdates:(NSArray *)updateItems {
+    NSLog(@"%@", updateItems);
+}
+
+@end
